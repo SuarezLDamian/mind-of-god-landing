57ad9693a1871b2d2bd6cb054ee30f64fa92a811
Leonardo Damian Suarez <suarezldamian@gmail.com>
2024-02-28 02:51:51 -0300

    fix: Change text size for small devices

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -4,7 +4,7 @@ import ReviewCard from './ReviewCard';
 const Reviews = () => {
   return (
     <section id="reviews" className="px-12 py-24 grid place-items-center bg-slate-900">
-      <h2 className="font-lato text-5xl font-bold text-gray-200">TESTIMONIOS</h2>
+      <h2 className="font-lato text-4xl md:text-5xl font-bold text-gray-200">TESTIMONIOS</h2>
       <div className="py-12">
         <ReviewCard title="Muy bueno" description="La calidad y el diseño son excelentes" />
       </div>
